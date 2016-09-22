912afef7831ea45f729e354b27a68de3ce5ab6d4
Pavel Naydenov <naydenov.p.v@gmail.com>
2016-09-22 10:43:40 +0300

    working example

--- tg_bot_for_wa.py
+++ tg_bot_for_wa.py
@@ -1,6 +1,14 @@
 import telegram
+from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters
+
 from datetime import datetime
 import configparser
+import threading, inspect, shlex
+try:
+    import Queue
+except ImportError:
+    import queue as Queue
 
 import os
 import sys
@@ -12,6 +20,8 @@ from yowsup.layers.network                             import YowNetworkLayer
 from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
 from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
 from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
+from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
+from yowsup.common.tools import Jid
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -20,16 +30,28 @@ tg_token = config.get('access', 'tg_token')
 tg_chat = config.get('access', 'tg_chat')
 wa_phone = config.get('access', 'wa_phone')
 wa_pass = config.get('access', 'wa_pass')
+wa_chat = config.get('access', 'wa_chat')
+wa_send_queue = Queue.Queue()
 
 class EchoLayer(YowInterfaceLayer):
+    def __init__(self):
+        super(EchoLayer, self).__init__()
+
+        self.listenQueueThread = threading.Thread(target = self.listenSendQueue)
+        self.listenQueueThread.daemon = True
+        self.listenQueueThread.start()
+
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
         receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom(), \
                                                 'read', messageProtocolEntity.getParticipant())
-
+        # import ipdb; ipdb.set_trace()
         # outgoingMessageProtocolEntity = TextMessageProtocolEntity(
         #     messageProtocolEntity.getBody(),
         #     to = messageProtocolEntity.getFrom())
+
+        if messageProtocolEntity.getBody() == 'ping':
+            self.getProp('send_queue').put('pong')
 
         self.toLower(receipt)
         self.getProp('telegram').sendMessage(chat_id=tg_chat, text=messageProtocolEntity.getBody())
@@ -40,10 +62,35 @@ class EchoLayer(YowInterfaceLayer):
         ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
         self.toLower(ack)
 
+    def listenSendQueue(self):
+        while not self.getStack():
+            pass
+        sendQueue = self.getProp('send_queue')
+        while True:
+            msg = sendQueue.get(True)
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(msg, to = Jid.normalize(self.getProp('wa_chat')))
+            # TextMessageProtocolEntity(content.encode("utf-8") if sys.version_info >= (3,0) else content, to = self.aliasToJid(number))
+            self.toLower(outgoingMessageProtocolEntity)
+
+
 if __name__==  "__main__":
     starte_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     tg = telegram.Bot(token=tg_token)
-    tg.sendMessage(chat_id=tg_chat, text="WathsApp bot started at %s" % starte_time)
+    #tg.sendMessage(chat_id=tg_chat, text="WathsApp bot started at %s" % starte_time)
+
+    def tg_message(bot, update):
+        #print "Tg msg: chat_id=%s, text=%s" % (str(update.message.chat_id), update.message.text)
+        wa_send_queue.put(update.message.text)
+
+    def tg_listen():
+        tg_updater = Updater(bot=tg)
+        echo_handler = MessageHandler([Filters.text], tg_message)
+        tg_updater.dispatcher.add_handler(echo_handler)
+        tg_updater.start_polling()
+
+    tgThread = threading.Thread(target = tg_listen)
+    tgThread.daemon = True
+    tgThread.start()
 
     stackBuilder = YowStackBuilder()
 
@@ -53,6 +100,8 @@ if __name__==  "__main__":
         .build()
 
     stack.setProp('telegram', tg)
+    stack.setProp('send_queue', wa_send_queue)
+    stack.setProp('wa_chat', wa_chat)
     stack.setCredentials((wa_phone, wa_pass))
     stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))
     try:
